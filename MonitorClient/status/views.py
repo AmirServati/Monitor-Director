6fa60acc032be9cc81fcdb6a7b2e58090fe80d9a
mehrabadairport <mehrabadinternationalairport@gmail.com>
2024-03-16 21:08:09 +0330

    monitor switch fix 3

--- MonitorClient/status/views.py
+++ MonitorClient/status/views.py
@@ -13,20 +13,23 @@ HDMI = {
 }
 
 # Create your views here.
+def switch_to(id):
+    monitor = Monitor.objects.get(id = 1)
+    monitor.source = HDMI[id]
+    monitor.save()
+    if id == 1:
+        os.system('echo "tx 4f:82:20:00" | cec-client -s -d 1')
+    else:
+        os.system('echo "as" | cec-client -s -d 1')
+    return "done"
+
 def status(request):
     source = Monitor.objects.get(id = 1).source
     return HttpResponse(source)
 
 def switch(request, id):
+    switch_to(id)
     monitor = Monitor.objects.get(id = 1)
-    monitor.source = HDMI[id]
-    monitor.save()
-    if id == 1:
-        os.system("wget http://%s:8000/status/switch/1 --delete-after" % IP)
-        os.system('echo "tx 4f:82:20:00" | cec-client -s -d 1')
-    else:
-        os.system("wget http://%s:8000/status/switch/2 --delete-after" % IP)
-        os.system('echo "as" | cec-client -s -d 1')
     return HttpResponse(monitor.source)
 
 def autoplay(request):
@@ -37,14 +40,14 @@ def autoplay(request):
     days            = tuple(map(int, request.GET['days'].split(',')))
     playlist        = request.GET['playlist']
     cron = CronTab(user=True)
-    playjob = cron.new(command='echo "as" | cec-client -s -d 1; wget http://%s:8000/status/switch/2 --delete-after; DISPLAY=:0.0 vlc http://192.168.5.58:80/Playlists/xspf/%s.xspf --fullscreen --loop' % (IP, playlist))
+    playjob = cron.new(command='echo "as" | cec-client -s -d 1; DISPLAY=:0.0 vlc http://192.168.5.58:80/Playlists/xspf/%s.xspf --fullscreen --loop' % playlist)
     play_comment = "PLAY->%s@%s-%s:%s" % (playlist, request.GET['days'], hour_start, minute_start)
     playjob.set_comment(play_comment)
     playjob.minute.on(minute_start)
     playjob.hour.also.on(hour_start)
     for day in days:
         playjob.dow.also.on(day)
-    exitjob = cron.new(command='echo "tx 4f:82:20:00" | cec-client -s -d 1; killall -9 vlc; wget http://%s:8000/status/switch/1 --delete-after' % IP)
+    exitjob = cron.new(command='echo "tx 4f:82:20:00" | cec-client -s -d 1; killall -9 vlc')
     exit_comment = "EXIT->%s@%s-%s:%s" % (playlist, request.GET['days'], hour_finish, minute_finish)
     exitjob.set_comment(exit_comment)
     exitjob.minute.on(minute_finish)
