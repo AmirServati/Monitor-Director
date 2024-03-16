49138f7b22b2a52e336f6c8bbce1272be09ec4a6
mehrabadairport <mehrabadinternationalairport@gmail.com>
2024-03-16 19:46:53 +0330

    first deploy

--- MonitorClient/status/views.py
+++ MonitorClient/status/views.py
@@ -37,14 +37,14 @@ def autoplay(request):
     days            = tuple(map(int, request.GET['days'].split(',')))
     playlist        = request.GET['playlist']
     cron = CronTab(user=True)
-    playjob = cron.new(command='echo "as" | cec-client -s -d 1; DISPLAY=:0.0 vlc http://192.168.5.58:80/Playlists/xspf/%s.xspf --fullscreen --loop' % playlist)
+    playjob = cron.new(command='echo "as" | cec-client -s -d 1; curl http://%s:8000/status/switch/2 ; DISPLAY=:0.0 vlc http://192.168.5.58:80/Playlists/xspf/%s.xspf --fullscreen --loop' % (IP, playlist))
     play_comment = "PLAY->%s@%s-%s:%s" % (playlist, request.GET['days'], hour_start, minute_start)
     playjob.set_comment(play_comment)
     playjob.minute.on(minute_start)
     playjob.hour.also.on(hour_start)
     for day in days:
         playjob.dow.also.on(day)
-    exitjob = cron.new(command='echo "tx 4f:82:20:00" | cec-client -s -d 1; killall -9 vlc')
+    exitjob = cron.new(command='echo "tx 4f:82:20:00" | cec-client -s -d 1; killall -9 vlc; curl http://%s:8000/status/switch/1' % IP)
     exit_comment = "EXIT->%s@%s-%s:%s" % (playlist, request.GET['days'], hour_finish, minute_finish)
     exitjob.set_comment(exit_comment)
     exitjob.minute.on(minute_finish)
